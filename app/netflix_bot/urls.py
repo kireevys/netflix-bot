64a4972c8782c773a21952d8b8eff02fd46e91e2
kireev <kireev@reg.ru>
2020-06-14 15:12:46 +0300

    enpoint

--- app/netflix_bot/urls.py
+++ app/netflix_bot/urls.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .views import CommandReceiveView
 
 urlpatterns = [
-    re_path(r'bot/(.*)/', CommandReceiveView.as_view()),
+    re_path(r'bot/', CommandReceiveView.as_view()),
 ]
