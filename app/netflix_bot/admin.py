96cbd6af7bfe5297f61d20b750b8b9e1c6e18e30
kireev <kireev@reg.ru>
2020-08-23 17:04:49 +0300

    enable admin

--- app/netflix_bot/admin.py
+++ app/netflix_bot/admin.py
@@ -1,1 +1,18 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import User, Episode, Series
+
+
+@admin.register(Episode)
+class EpisodeAdmin(admin.ModelAdmin):
+    exclude = ('file_id', 'message_id')
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    exclude = ('poster', )
