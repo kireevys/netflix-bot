758196952999d4ec8c29701deefbe2889621e780
kireev yuriy <kireev_y@tochka.com>
2021-07-25 04:10:13 +0300

    Починил счетчик рефералок

--- app/netflix_bot/telegram_bot/commands.py
+++ app/netflix_bot/telegram_bot/commands.py
@@ -9,6 +9,7 @@ from ..models import Referral, User
 from .managers.movie import MovieCallback
 from .managers.series import SeriesCallback
 from .senders import InlineSender
+from .user_interface.callbacks import CallbackManager
 from .user_interface.router import Route, router
 
 logger = logging.getLogger(__name__)
@@ -31,19 +32,26 @@ def series(update: Update, context: CallbackContext):
 
 
 def start(update: Update, context: CallbackContext):
-    if context.args:
-        try:
-            query = Route.b64decode(context.args[0])
-            handler, args = router.get_handler(query)
-            m = MovieCallback(update, context, InlineSender(update, context))
-            handler(m, *args)
-            return
-        except (binascii.Error, UnicodeDecodeError):
-            pass
-    else:
-        MovieCallback(update, context, InlineSender(update, context)).root()
-
+    manager = MovieCallback(update, context, InlineSender(update, context))
     user, created = User.get_or_create(update.effective_user)
-    if context.args and created:
-        logger.info(f"new user {user} created")
-        Referral.add(context.args[0], user)
+
+    if not context.args:
+        manager.root()
+
+    # Если есть аргументы, то это либо путь, либо рефералка.
+    if not handle_path(context.args[0], manager):
+        # Если это не путь, значит рефералка.
+        # Рефералов считает только для новых юзеров
+        if created:
+            Referral.add(context.args[0], user)
+
+
+def handle_path(args: str, manager: CallbackManager) -> bool:
+    try:
+        query = Route.b64decode(args)
+    except (binascii.Error, UnicodeDecodeError):
+        return False
+
+    handler, args = router.get_handler(query)
+    handler(manager, *args)
+    return True
