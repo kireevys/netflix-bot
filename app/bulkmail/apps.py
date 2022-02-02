516af745bfc4813efefdd686adcf5c39a0308c4b
kireev yuriy <kireev_y@tochka.com>
2022-02-02 14:29:41 +0300

    bulkmail core
    - Django app

---
+++ app/bulkmail/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BulkmailConfig(AppConfig):
+    name = 'bulkmail'
--- app/project/settings.py
+++ app/project/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "netflix_bot.apps.ProjectConfig",
     "master.apps.MasterConfig",
+    "bulkmail.apps.BulkmailConfig",
 ]
 
 MIDDLEWARE = [
